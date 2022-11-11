312150416e005b247fdcef4bfb235f9d48de7d3b
loony <wooounding@gmail.com>
2022-11-11 16:28:06 +0300

    поправил ошибка линтера

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,10 @@
-from django.contrib.auth.views import *
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordChangeDoneView,
+                                       PasswordChangeView,
+                                       PasswordResetCompleteView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetDoneView,
+                                       PasswordResetView)
 from django.urls import path
 
 from . import views
@@ -9,42 +15,54 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
         'logout/',
-        LogoutView.as_view(template_name='users/logged_out.html'), 
+        LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'), 
+        LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'), 
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
+        ),
         name='password_change_form'
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), 
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
         name='password_change_done'
     ),
     path(
         'password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'), 
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
         name='password_reset_form'
     ),
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
         name='password_reset_confirm'
     ),
     path(
         'reset/done/',
-        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
         name='password_reset_complete'
     ),
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -131,4 +131,4 @@ LOGIN_REDIRECT_URL = 'posts:index'
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 # указываем директорию, в которую будут складываться файлы писем
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails') 
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
